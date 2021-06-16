342aa5d8c50106150cf6a2919d7c1a1e41260564
Jukka Keski-Luopa <jukka.keskiluopa@taitounited.fi>
2021-06-16 16:27:09 +0300

    fix: remove fact_sales rows from example data

--- lab/lessons/files/db/data/dev.sql
+++ lab/lessons/files/db/data/dev.sql
@@ -29,27 +29,3 @@ INSERT INTO dim_products (
   'Guitars',
   'Electric guitars'
 );
-
--- Add some sales
-
-INSERT INTO fact_sales (
-  key, date_key, product_key, order_number, quantity, price
-) values (
-  '00000000001.1-1',
-  '2021-02-26',
-  '1-1',
-  '00000000001',
-  1,
-  2129.00
-);
-
-INSERT INTO fact_sales (
-  key, date_key, product_key, order_number, quantity, price
-) values (
-  '00000000002.1-2',
-  '2021-03-14',
-  '1-2',
-  '00000000002',
-  2,
-  2659.00
-);
